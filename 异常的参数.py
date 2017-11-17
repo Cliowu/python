7bbc97b337d66fe5cd542ae9b0ece1d7d6c0b972
Wuhaiyang27 <33240061+Cliowu@users.noreply.github.com>
2017-11-17 14:11:50 +0800

    Delete 异常的参数.py

--- 异常的参数.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-def temp_convert(var):
-    try:
-        return int(var)
-    except ValueError, Argument:
-        print "parameter does not contain numbers\n", Argument
